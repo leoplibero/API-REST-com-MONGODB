939b63375ba3d49555009b107fee9c0d925a6f62
Leonardo <leonardop.libero@hotmail.com>
2024-06-05 18:19:12 -0300

    Main

---
+++ express-mongo/src/app.js
@@ -0,0 +1,52 @@
+import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+import livro from "./models/Livro.js"
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (error) =>{
+    console.error("Erro de conexão!", erro);//metodo para imprimir erro no log
+})
+
+conexao.once("open", () =>{
+    console.log("Conexão com banco efetuada com sucesso! ")
+})
+
+const app = express();
+app.use(express.json());// Middleware para parsear JSON
+
+
+
+app.get("/", (req, res) => {
+    res.status(200).send("Curso de Node.js");
+});
+
+app.get("/livros/id:", (req, res) =>{
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+})
+
+
+app.get("/livros", async (req,res) =>{
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
+})
+
+app.post ("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso")
+})
+
+app.put("/livros/:id", (req, res) =>{
+    const index=buscaLivro (req.params.id);
+    livros[index].titulo=req.body.titulo;
+    res.status(200).json(livros)
+})
+
+app.delete("/livros/:id", (req, res) =>{
+    const index=buscaLivro (req.params.id);
+    livros.splice(index, 1);//deletar 1 elemento id
+    res.status(200).send("Livro Removido com Sucesso");
+})
+
+export default app;
